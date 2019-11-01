32727d0cf009a13d32b9c1a9e9b214886e34d749
BG <benny.gu@sap.com>
2019-11-01 20:00:05 +0800

    no message

--- enable_Hypter-V_in_WIN10Home.bat
+++ enable_Hypter-V_in_WIN10Home.bat
@@ -3,3 +3,6 @@ dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum >hyper-v.txt
 for /f %%i in ('findstr /i . hyper-v.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
 del hyper-v.txt
 dism /online /enable-feature /featurename:Microsoft-Hyper-V-All /LimitAccess /ALL
+
+REM bcdedit /set hypervisorlaunchtype off
+REM bcdedit /set hypervisorlaunchtype auto
